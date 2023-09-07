0870bda39e05b565e913af60ea648f033b517b54
Nauris <nauris.gruduls@gmail.com>
2023-09-08 01:51:40 +0300

    walkthrough changes

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -3,20 +3,24 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
-window.title("flashy")
+window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, highlightthickness=0)
-front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 264, image=front_img)
+card_front = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front)
+canvas.config(bg=BACKGROUND_COLOR)
 canvas.grid(row=0, column=0, columnspan=2)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 
 #Buttons
-wrong_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
-wrong_button.grid(row=1, column=0)
-right_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_img, highlightthickness=0)
-right_button.grid(row=1, column=1)
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+
+check_img = PhotoImage(file="images/right.png")
+known_button = Button(image=check_img, highlightthickness=0)
+known_button.grid(row=1, column=1)
 
 window.mainloop()
