4d68683cd0d1afcd17555320d66407ac325da2a8
Nauris <nauris.gruduls@gmail.com>
2023-10-01 19:07:25 +0300

    step 4 done

--- health-tracking/main.py
+++ health-tracking/main.py
@@ -1,10 +1,15 @@
+import json
+
 import requests
+from datetime import datetime
 
 APP_ID = "9ea1820c"
 API_KEY = "94b4986f3bc5f47f610cfe0ed3104511"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-exercise_text = input("what did you do (exercise)?")
+sheet_endpoint = "https://api.sheety.co/464e0305c4e96ea7d2620795c3df0634/workoutTracking/workouts"
+
+exercise_text = input("what did you do (exercise): ")
 
 headers = {
     "x-app-id": APP_ID,
@@ -23,4 +28,22 @@ parameters = {
 
 response = requests.post(url=exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
-print(result)
+
+#########################################
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
