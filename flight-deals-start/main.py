664a3ae8d455b23e24363c3a7157730c39948b6a
Nauris <nauris.gruduls@gmail.com>
2023-10-21 12:23:15 +0300

    step 5

--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,11 +1,13 @@
 from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 print(f"intiial sheet data: {sheet_data}")
 flight_search = FlightSearch()
+notification_manager = NotificationManager()
 
 ORIGIN_CITY_IATA = "LON"
 
@@ -26,3 +28,8 @@ for destination in sheet_data:
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only ${flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}"
+        )
